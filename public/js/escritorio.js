e73aa03fe2d01efe1b6643234a45120b4413a076
samdev <samuelberrus@gmail.com>
2021-12-21 13:19:31 +0100

    Configurado js básico de escritorio en el front

--- public/js/escritorio.js
+++ public/js/escritorio.js
@@ -1,3 +1,31 @@
+//Referencias
+const lblEscritorio = document.querySelector("h1");
+const btnAtender = document.querySelector("button");
 
+//Params
+const searchParams = new URLSearchParams(window.location.search);
+//Existe param escritorio
+if (!searchParams.has("escritorio")) {
+	window.location = "index.html";
+	throw new Error("El escritorio es obligatorio");
+}
 
-console.log('Escritorio HTML');
+//Obtenemos el valor del param escritorio y lo asignamos al primer h1
+const escritorio = searchParams.get("escritorio");
+lblEscritorio.innerText = escritorio;
+
+//Sockets
+const socket = io();
+
+socket.on("connect", () => {
+	btnAtender.disabled = false;
+});
+socket.on("disconnect", () => {
+	btnAtender.disabled = true;
+});
+
+socket.on("connect", () => {
+	btnAtender.addEventListener("click", () => {
+		//
+	});
+});
